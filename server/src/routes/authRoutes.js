f8ffcea726ba24d96cd763bb006b55579392b40c
Rezgui Yassine <rezguiyassine224@gmail.com>
2024-05-07 04:11:17 +0200

    swagger of crudUser , authentification and quizRouter

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -50,6 +50,89 @@ const authRouter = express.Router();
  *         description: User created successfully
  */
 
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *    tags: ["auth"]
+ *    summary: Login to the application
+ *    description: Login to the application
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: User logged in successfully
+ *      '401':
+ *        description: Invalid credentials
+ *      '500':
+ *        description: Server error
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /api/auth/me:
+ *  get:
+ *    tags: ["auth"]
+ *    summary: Get current user by token
+ *    description: Get current user by token
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      '200':
+ *        description: A user object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                user:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: string
+ *                    firstName:
+ *                      type: string
+ *                    lastName:
+ *                      type: string
+ *      '404':
+ *        description: User not found
+ *      '401':
+ *        description: Unauthorized
+ */
+
+/**
+ * @swagger
+ * /api/auth/verif/{activationCode}:
+ *  post:
+ *    tags: ["auth"]
+ *    summary: Verify user
+ *    description: Verify user
+ *    parameters:
+ *      - in: path
+ *        name: activationCode
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: User verified successfully
+ */
+
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
 authRouter.get("/me", authentificateToken);
